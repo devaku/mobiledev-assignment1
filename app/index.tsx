43bf468bb26dcb8d6af354bad8f5fb251caed8ff
Alejo Kim Uy <alejokimuy@gmail.com>
2025-05-27 14:36:58 -0600

    Testing

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import '../assets/css/global.css';
 
-export default function App() {
-    return (
-        <View>
-            <Text className="bg-sky-900 text-white">
-                Open up App.tsx to start working on your app!
-            </Text>
-            <StatusBar style="auto" />
-        </View>
-    );
+export default function MyComponent() {
+	return (
+		<View className="flex-1 items-center justify-center ">
+			<Text className="font-bold text-4xl text-orange-600">
+				Some text
+			</Text>
+		</View>
+	);
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
