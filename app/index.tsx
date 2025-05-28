b3a634b90fa964eeede7f7ca6f81336af568526e
Alejo Kim Uy <alejokimuy@gmail.com>
2025-05-27 19:19:47 -0600

    Start of carousel

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+import { Pressable, Button } from 'react-native';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import '../assets/css/global.css';
 
@@ -22,28 +22,46 @@ export default function App() {
 				<View className="flex flex-row gap-2">
 					<View>
 						<Text className="text-center">69</Text>
-						<Text>Posts</Text>
+						<Text className="font-bold">Posts</Text>
 					</View>
 					<View>
 						<Text className="text-center">420</Text>
-						<Text>Members</Text>
+						<Text className="font-bold">Members</Text>
 					</View>
 					<View>
 						<Text className="text-center">-1</Text>
-						<Text>Admins</Text>
+						<Text className="font-bold">Admins</Text>
 					</View>
 				</View>
 			</View>
 
 			{/* Text */}
+			<View className="ml-5 my-2">
+				<Text>OOTD Everyday</Text>
+				<Text>Fit Check! 👚</Text>
+				<Text>You know we'll hype you up.</Text>
+			</View>
 
 			{/* Member Button */}
+			<View className="flex flex-row justify-center border border-slate-400 p-1 m-4">
+				<Pressable>
+					<View>
+						<Text className="font-semibold">Member v</Text>
+					</View>
+				</Pressable>
+			</View>
 
 			{/* Carousel of Images */}
-
-			<Text className="bg-sky-900 text-white">
-				Open up App.tsx to start working on your app!
-			</Text>
+			<View>
+				<Image
+					className="rounded-full"
+					style={{
+						width: 100,
+						height: 100,
+					}}
+					source={require('../assets/images/deadserver.gif')}
+				></Image>
+			</View>
 		</View>
 	);
 }
