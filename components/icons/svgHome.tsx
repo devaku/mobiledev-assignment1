2308798806e94da1b3f00839046e2f2667945e91
Alejo Kim Uy <alejokimuy@gmail.com>
2025-05-28 13:50:22 -0600

    Added svgs

---
+++ components/icons/svgHome.tsx
@@ -0,0 +1,8 @@
+import Svg, { Path } from 'react-native-svg';
+export default function SvgHome() {
+	return (
+		<Svg width="24" height="24" viewBox="0 0 24 24">
+			<Path d="M12 6.453l9 8.375v9.172h-6v-6h-6v6h-6v-9.172l9-8.375zm12 5.695l-12-11.148-12 11.133 1.361 1.465 10.639-9.868 10.639 9.883 1.361-1.465z" />
+		</Svg>
+	);
+}
